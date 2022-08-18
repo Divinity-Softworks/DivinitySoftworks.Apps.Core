d2286c7f6b4af4f2efef4c8927a6194e1a5e2786
Michael Keeman <m.keeman@niped.nl>
2022-08-18 08:32:30 +0200

    Small Fixes.

--- DivinitySoftworks.Apps.Core/Converters/EnumToVisibilityConverter.cs
+++ DivinitySoftworks.Apps.Core/Converters/EnumToVisibilityConverter.cs
@@ -7,36 +7,31 @@ using System.Windows.Markup;
 namespace DivinitySoftworks.Apps.Core.Converters {
 
     /// <summary>
-    /// Converts an <seealso cref="Enum"/> to <seealso cref="Visibility"/>.
+    /// Converts an <seealso cref="Enum"/> to a <seealso langword="bool"/>.
     /// <para>
     /// To use this converter. Add <c>xmlns:converters="clr-namespace:DivinitySoftworks.Apps.Core.Converters;assembly=DivinitySoftworks.Apps.Core"</c> to the XAML file.
     /// </para>
     /// </summary>
-    public class EnumToBooleanConverter : MarkupExtension, IValueConverter {
+    public class EnumToVisibilityConverter : MarkupExtension, IValueConverter {
 
         /// <summary>
-        /// The <seealso cref="Enum"/> the value should be.
+        /// The <seealso cref="Enum"/> the value should be to be <seealso cref="Visibility.Visible"/>.
         /// </summary>
-        public Enum? True { get; set; }
+        public Enum? Visible { get; set; }
 
         /// <inheritdoc/>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (True is null) throw new NullReferenceException(nameof(True));
+            if (Visible is null) throw new NullReferenceException(nameof(Visible));
 
             if (value is not null && value.GetType().IsEnum)
-                return Equals(value, True);
+                return Equals(value, Visible) ? Visibility.Visible : Visibility.Collapsed;
             else
                 return DependencyProperty.UnsetValue;
         }
 
         /// <inheritdoc/>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture) {
-            if (True is null) throw new NullReferenceException(nameof(True));
-
-            if (value is not null && value is bool boolean && boolean)
-                return True;
-            else
-                return DependencyProperty.UnsetValue;
+            throw new NotImplementedException();
         }
 
         /// <inheritdoc/>
@@ -45,3 +40,4 @@ namespace DivinitySoftworks.Apps.Core.Converters {
         }
     }
 }
+
